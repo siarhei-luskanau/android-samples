080bae7736d676d2d023bab09517eb532de58bbb
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2017-06-12 10:50:16 +0300

    Firebase database: small updates

--- firebase-database/app/src/main/java/siarhei/luskanau/example/firebase/database/MainActivity.java
+++ firebase-database/app/src/main/java/siarhei/luskanau/example/firebase/database/MainActivity.java
@@ -3,6 +3,7 @@ package siarhei.luskanau.example.firebase.database;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -10,40 +11,48 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private TextView messageTextView;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        messageTextView = (TextView) findViewById(R.id.messageTextView);
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = database.getReference("message");
 
         writeToDatabase(myRef);
         readFromDatabase(myRef);
     }
 
-    private void writeToDatabase(DatabaseReference myRef) {
+    private void writeToDatabase(final DatabaseReference myRef) {
         // Write a message to the database
-        myRef.setValue("Hello, World!");
+        myRef.setValue("Hello, World! " + new Date());
     }
 
-    private void readFromDatabase(DatabaseReference myRef) {
+    private void readFromDatabase(final DatabaseReference myRef) {
         // Read from the database
         myRef.addValueEventListener(new ValueEventListener() {
+
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
+            public void onDataChange(final DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
+                final String value = dataSnapshot.getValue(String.class);
+                messageTextView.setText(value);
                 Log.d(TAG, "Value is: " + value);
             }
 
             @Override
-            public void onCancelled(DatabaseError error) {
+            public void onCancelled(final DatabaseError error) {
                 // Failed to read value
                 Log.w(TAG, "Failed to read value.", error.toException());
             }
