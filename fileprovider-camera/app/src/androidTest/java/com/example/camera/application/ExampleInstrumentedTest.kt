5806a5269325297039d0fea6088298862f4ce340
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2018-04-04 11:31:35 +0300

    - Added InstrumentedTest. - Updated dependencies.

---
+++ fileprovider-camera/app/src/androidTest/java/com/example/camera/application/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.example.camera.application
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals(BuildConfig.APPLICATION_ID + "", appContext.packageName)
+    }
+
+}
