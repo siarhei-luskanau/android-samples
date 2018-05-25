cbefbadff4b2ade2a8fc39d5b4c2d9e774b8a2f3
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2018-05-26 00:53:40 +0300

    Converted to kotlin. Updated to android-x from android-support.

---
+++ fileprovider-camera/app/src/main/java/com/example/camera/application/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.camera.application
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.camera.library.CameraUtils
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var imageUriTextView: TextView
+    private lateinit var imageView: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        imageUriTextView = findViewById(R.id.imageUriTextView)
+        imageView = findViewById(R.id.imageView)
+
+        findViewById<View>(R.id.cameraButton).setOnClickListener {
+            val takePictureIntent = CameraUtils.createCameraIntent(applicationContext)
+            if (takePictureIntent.resolveActivity(packageManager) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+
+        findViewById<View>(R.id.galleryButton).setOnClickListener {
+            val takePictureIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            if (takePictureIntent.resolveActivity(packageManager) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+
+        showImageUri(null)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val uri = if (data != null) {
+                data.data
+            } else {
+                CameraUtils.getCameraTempFileProviderUri(applicationContext)
+            }
+            showImageUri(uri)
+        } else {
+            showImageUri(null)
+        }
+    }
+
+    private fun showImageUri(uri: Uri?) {
+        imageUriTextView.text = String.format(Locale.ENGLISH, "Uri: %s", uri.toString())
+        imageView.setImageURI(uri)
+    }
+
+    companion object {
+        private const val REQUEST_IMAGE_CAPTURE = 1
+    }
+}
---
+++ fileprovider-camera/camera/src/main/java/com/example/camera/library/FileProviderUtils.kt
@@ -0,0 +1,32 @@
+package com.example.camera.library
+
+import android.content.Context
+import android.net.Uri
+import androidx.core.content.FileProvider
+
+import java.io.File
+
+object FileProviderUtils {
+
+    private const val AUTHORITIES_SUFFIX = ".provider"
+    private const val FILE_PROVIDER_PATHS = "temp"
+
+    fun getFileProviderUri(context: Context, fileName: String): Uri =
+            FileProvider.getUriForFile(context,
+                    context.packageName + AUTHORITIES_SUFFIX,
+                    createFile(context, fileName)
+            )
+
+
+    fun deleteFile(context: Context, fileName: String) =
+            createFile(context, fileName).delete()
+
+
+    private fun createFile(context: Context, fileName: String): File {
+        val fileProviderDir = File(context.filesDir, FILE_PROVIDER_PATHS)
+        if (!fileProviderDir.exists()) {
+            fileProviderDir.mkdirs()
+        }
+        return File(fileProviderDir, fileName)
+    }
+}
