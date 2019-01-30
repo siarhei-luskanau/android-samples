b4b33ed77701f884edca0048c59f0c0518306b55
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2019-01-30 03:14:16 +0300

    Updated Navigation example project.

--- navigation/app/src/main/java/siarhei.luskanau.example.navigation/Feature1Fragment.kt
+++ navigation/app/src/main/java/siarhei.luskanau.example.navigation/Feature1Fragment.kt
@@ -1,5 +1,27 @@
 package siarhei.luskanau.example.navigation
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import siarhei.luskanau.example.navigation.databinding.FragmentFeature1Binding
 
-class Feature1Fragment : Fragment()
+class Feature1Fragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentFeature1Binding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (requireActivity() as AppCompatActivity).supportActionBar?.title = javaClass.simpleName
+    }
+}
